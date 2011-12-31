6dc303aab22bd272cb3272bdf9926f896221d431
Ben Armstrong <synrg@debian.org>
2011-12-31 15:35:30 -0400

    Write cli specs.

--- spec/stream2tracks/cli_spec.rb
+++ spec/stream2tracks/cli_spec.rb
@@ -1,26 +1,35 @@
 require 'stream2tracks/cli'
+require 'stringio'
 require 'tempfile'
 
 describe 'stream2tracks' do
+    before :each do
+	$stderr=StringIO.new
+	$stdout=StringIO.new
+    end
     it 'should require a filename' do
-	pending()
+	lambda {stream2tracks ['/dev/null']}.should_not raise_error
+	lambda {stream2tracks []}.should raise_error(SystemExit)
+	$stdout.string.should match /^Usage:/
     end
     it 'should display help' do
-	pending()
+	lambda {stream2tracks ['--help']}.should raise_error(SystemExit)
+	$stdout.string.should match /^Usage:/
     end
     it 'should display version' do
-	pending()
+	lambda {stream2tracks ['--version','/dev/null']}.should raise_error(SystemExit)
+	$stdout.string.should match /^\d+\.\d+\.\d+$/
     end
     it 'should accept format' do
-	pending()
+	lambda {stream2tracks ['--format','ogg','/dev/null']}.should_not raise_error
     end
     it 'should accept log' do
-	pending()
+	lambda {stream2tracks ['--log','/dev/null','/dev/null']}.should_not raise_error
     end
     it 'should accept quiet' do
-	pending()
+	lambda {stream2tracks ['--quiet','/dev/null']}.should_not raise_error
     end
     it 'should accept multi' do
-	pending()
+	lambda {stream2tracks ['--multi','/dev/null']}.should_not raise_error
     end
 end
