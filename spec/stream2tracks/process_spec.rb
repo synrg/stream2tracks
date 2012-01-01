2efd776cde7d96119af9ed4efbe79362e81d6c2b
Ben Armstrong <synrg@debian.org>
2012-01-01 16:03:14 -0400

    Specs for multiple processes, progress bar.

--- spec/stream2tracks/process_spec.rb
+++ spec/stream2tracks/process_spec.rb
@@ -2,13 +2,16 @@ require 'stream2tracks/process'
 require 'ostruct'
 
 TEST_CMD=%[for i in $(seq 3); do echo $i; sleep .01; done; echo done]
-TEST_VALIDATE=proc{|buf|/done/.match(buf) ? true : false}
+TEST_VALIDATE=proc{|buf|$out << buf ; /done/.match(buf) ? true : false}
 TEST_PROGRESS=proc{|buf|cur=0; buf.scan(/\d+/).each{|i|cur+=i.to_i} ; [cur,6]}
 TEST_CURRENT=[1,3,6,6]
+TEST_SINGLE=%[1\n2\n3\ndone\n]
+TEST_MULTI=%[1\n1\n1\n2\n1\n2\n1\n2\n3\n1\n2\n3\n1\n2\n3\ndone\n1\n2\n3\ndone\n]
 
 describe WatchedProcess do
     describe '#watch' do
 	before :each do
+	    $out=''
 	    @process=WatchedProcess.new TEST_CMD
 	end
 	it 'should validate output' do
@@ -41,22 +44,27 @@ end
 describe WatchedProcessGroup do
     describe '#watch' do
 	before :each do
+	    $out=''
 	    $stderr=StringIO.new
+	    @options=OpenStruct.new
+	    @options.validate=TEST_VALIDATE
+	    @options.progress=TEST_PROGRESS
+	    @processes=WatchedProcessGroup.new
 	end
 	it 'should run a single process' do
-	    options=OpenStruct.new
-	    options.validate=TEST_VALIDATE
-	    options.progress=TEST_PROGRESS
-	    processes=WatchedProcessGroup.new
-	    processes << WatchedProcess.new(TEST_CMD)
-	    processes.watch options
-	    $stderr.string.should match(/100%/)
+	    @processes << WatchedProcess.new(TEST_CMD)
+	    @processes.watch @options
+	    $out.should match(TEST_SINGLE)
 	end
 	it 'should run multiple processes concurrently' do
-	    pending()
+	    2.times{@processes << WatchedProcess.new(TEST_CMD)}
+	    @processes.watch @options
+	    $out.should match(TEST_MULTI)
 	end
 	it 'should display a progress bar' do
-	    pending()
+	    @processes << WatchedProcess.new(TEST_CMD)
+	    @processes.watch @options
+	    $stderr.string.should match(/100%/)
 	end
     end
 end
