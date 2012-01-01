575e22e7a970d700c7f6bd6e5aa72171357b612a
Ben Armstrong <synrg@debian.org>
2011-12-31 22:16:22 -0400

    Add specs for WatchedProcess.

--- spec/stream2tracks/process_spec.rb
+++ spec/stream2tracks/process_spec.rb
@@ -1,15 +1,35 @@
 require 'stream2tracks/process'
+require 'ostruct'
 
 describe WatchedProcess do
     describe '#watch' do
+	before :each do
+	    @process=WatchedProcess.new %[sh -c 'for i in $(seq 3) ; do echo $i ; sleep .01 ; done ; echo done']
+	end
 	it 'should validate output' do
-	    pending()
+	    options=OpenStruct.new
+	    options.validate=proc{|buf|/done/.match(buf) ? true : false}
+	    eof,status,count=nil,nil,0
+	    while !eof
+		status=@process.watch options
+		count+=1
+		eof=status.eof
+	    end
+	    count.should == 4
+	    status.valid.should == true
 	end
 	it 'should measure progress' do
-	    pending()
-	end
-	it 'should set eof when done' do
-	    pending()
+	    options=OpenStruct.new
+	    options.progress=proc{|buf|cur=0; buf.scan(/\d+/).each{|i|cur+=i.to_i} ; [cur,6]}
+	    eof,status,count,total=nil,nil,0,0
+	    progress=[1,3,6,6]
+	    while !eof
+		status=@process.watch options
+		status.current.should == progress[count]
+		count+=1
+		eof=status.eof
+	    end
+	    count.should == 4
 	end
     end
 end
